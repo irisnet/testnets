3741cc7a798842cdf27cbd1b83a13048be50cf44
kenian.shi <1035868500@qq.com>
2019-11-10 01:03:43 +0800

    Update verify.go
    增加一些注释

--- nyancat/v0.16/verify.go
+++ nyancat/v0.16/verify.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 const (
@@ -18,29 +19,32 @@ const (
 	task4TP2 = "btc-eth"
 )
 
-type validator struct {
-	moniker          string
-	operationAddress string
-	pgp              string
+type validator struct {				//用来将validator的pgp和ownkey进行匹配
+	moniker string
+	ownKey  string
+	pgp     string
 }
 
-func fetch(url string) ([]byte, error) {
-	client := http.Client{}
-	request, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
-	resp, err := client.Do(request)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	return ioutil.ReadAll(resp.Body)
+type taskDetail struct {			//每次任务查询后返回的原始结果
+	hash      string
+	sender    string
+	height    string
+	timestamp string
 }
 
-var validatorSet = make(map[string]validator)
+type taskStat struct {			//某个验证人四个任务是否完成
+	task1 bool
+	task2 bool
+	task3 bool
+	task4 bool
+}
 
+var validatorSet = make(map[string]validator)			//验证人集合
+var taskTotal = make(map[string]taskStat)				//总的任务汇总
+var notValidatorSender = make(map[string]taskStat)		//记录一些交易的发送者在验证人列表中找不到的信息
+var repeatTxMap = make(map[string][]taskDetail)			//记录一些相同任务重复做的交易
+
+//根据验证人的operationAddress从浏览器上面查找相应的ownerKey数据
 func getOwnerKey(fva string) string {
 	data, err := fetch("https://nyancat.irisplorer.io/api/stake/validators/" + fva)
 	if err != nil {
@@ -55,6 +59,7 @@ func getOwnerKey(fva string) string {
 	return ownerKey
 }
 
+//初始化是将验证人信息保存到本地的validatorSet集合
 func init() {
 	data, err := fetch("https://lcd.nyancat.irisnet.org/stake/validators")
 	if err != nil {
@@ -76,22 +81,16 @@ func init() {
 		} else {
 			vali.moniker = moniker
 			vali.pgp = pgp
-			vali.operationAddress = ownerAddress
+			vali.ownKey = ownerAddress
 			validatorSet[ownerAddress] = vali
 		}
 	}
 }
 
-type taskDetail struct {
-	sender    string
-	hash      string
-	height    string
-	timestamp string
-}
-
+//task1 task2的任务检查，输入url，返回为各个交易所需要的一些信息。
 func verifyTask12(url string) []taskDetail {
 	taskDetails := []taskDetail{}
-	data, err := fetch("https://lcd.nyancat.irisnet.org/txs?action=add_liquidity")
+	data, err := fetch(url)
 	if err != nil {
 		panic(err)
 	}
@@ -105,17 +104,15 @@ func verifyTask12(url string) []taskDetail {
 	for _, tx := range txs {
 		code := tx.(map[string]interface{})["result"].(map[string]interface{})["Code"].(float64)
 		if code != 0 {
-			continue
+			continue				//只记录成功的交易
 		}
-
 		hash := tx.(map[string]interface{})["hash"].(string)
 		height := tx.(map[string]interface{})["height"].(string)
 		timestamp := tx.(map[string]interface{})["timestamp"].(string)
 
-		//tags := tx.(map[string]interface{})["result"].(map[string]interface{})["Tags"].([]interface{})
 		tags := tx.(map[string]interface{})["result"].(map[string]interface{})["Tags"].([]interface{})
 		tagsMap := make(map[string]string)
-		for _, tag := range tags {
+		for _, tag := range tags {				//将tags解析保存到缓存中，去除json文件格式中的属性名
 			key := tag.(map[string]interface{})["key"].(string)
 			value := tag.(map[string]interface{})["value"].(string)
 			tagsMap[key] = value
@@ -125,7 +122,7 @@ func verifyTask12(url string) []taskDetail {
 				hash:      hash,
 				height:    height,
 				sender:    sender,
-				timestamp: timestamp,
+				timestamp: timestamp,				//获取交易的sender
 			}
 			//fmt.Println(detail)
 			taskDetails = append(taskDetails, detail)
@@ -134,6 +131,8 @@ func verifyTask12(url string) []taskDetail {
 	return taskDetails
 }
 
+
+//task3 task4 任务检查，输入url以及token-pairs名，由于可能存在btc-eth,或者eth-btc均可，返回完成任务交易的相关情况
 func verifyTask34(url, tokenPairs1, tokenPairs2 string) []taskDetail {
 	taskDetails := []taskDetail{}
 	data, err := fetch(url)
@@ -148,10 +147,9 @@ func verifyTask34(url, tokenPairs1, tokenPairs2 string) []taskDetail {
 	txs := m["txs"].([]interface{})
 	for _, tx := range txs {
 		code := tx.(map[string]interface{})["result"].(map[string]interface{})["Code"].(float64)
-		if code != 0 {
+		if code != 0 {		//只记录成功的交易
 			continue
 		}
-
 		hash := tx.(map[string]interface{})["hash"].(string)
 		height := tx.(map[string]interface{})["height"].(string)
 		timestamp := tx.(map[string]interface{})["timestamp"].(string)
@@ -164,7 +162,7 @@ func verifyTask34(url, tokenPairs1, tokenPairs2 string) []taskDetail {
 			tagsMap[key] = value
 		}
 
-		if tp, ok := tagsMap["token-pair"]; ok && (tp == tokenPairs1 || tp == tokenPairs2) {
+		if tp, ok := tagsMap["token-pair"]; ok && (tp == tokenPairs1 || tp == tokenPairs2) {     //需要核对token-pairs对是否正确
 			if sender, ok := tagsMap["sender"]; ok {
 				detail := taskDetail{
 					hash:      hash,
@@ -172,84 +170,101 @@ func verifyTask34(url, tokenPairs1, tokenPairs2 string) []taskDetail {
 					sender:    sender,
 					timestamp: timestamp,
 				}
-				//fmt.Println(detail)
 				taskDetails = append(taskDetails, detail)
 			}
 		}
 	}
-
 	return taskDetails
-
 }
 
-type taskStat struct {
-	task1 bool
-	task2 bool
-	task3 bool
-	task4 bool
-}
+
 
 func main() {
 
 	task1Details := verifyTask12(task1URL)
 	fmt.Printf("task1完成人数统计：%d \n", len(task1Details))
-
 	task2Details := verifyTask12(task2URL)
 	fmt.Printf("task2完成人数统计：%d \n", len(task2Details))
 	task3Details := verifyTask34(task3URL, task3TP1, task3TP2)
 	fmt.Printf("task3完成人数统计：%d \n", len(task3Details))
 	task4Details := verifyTask34(task4URL, task4TP1, task4TP2)
 	fmt.Printf("task4完成人数统计：%d \n", len(task4Details))
-	taskTotal := make(map[string]taskStat)
-	notValidatorSender := make(map[string]taskStat)
 
-	senderMatchPGP(1, task1Details, taskTotal, notValidatorSender)
-	senderMatchPGP(2, task2Details, taskTotal, notValidatorSender)
-	senderMatchPGP(3, task3Details, taskTotal, notValidatorSender)
-	senderMatchPGP(4, task4Details, taskTotal, notValidatorSender)
+	senderMatchPGP(1, task1Details)
+	senderMatchPGP(2, task2Details)
+	senderMatchPGP(3, task3Details)
+	senderMatchPGP(4, task4Details)
 
-	fmt.Printf("验证人人数统计：%d 人完成任务\n", len(taskTotal))
+	fmt.Printf("从验证人人数统计：%d 人完成任务情况：\n", len(taskTotal))
 	for k, v := range taskTotal {
 		fmt.Printf("%s %+v \n\n", k, v)
 	}
 
-	fmt.Printf("为从验证人找到人数统计：%d 人完成任务\n", len(notValidatorSender))
+	fmt.Printf("未从验证人中找到人数统计：%d 人完成任务\n", len(notValidatorSender))
 	for k, v := range notValidatorSender {
 		fmt.Printf("%s %+v \n\n", k, v)
 	}
 
+	for k,v := range repeatTxMap {
+		fmt.Printf("sender:%s 重复交易次数：%d\n",k,len(v))
+	}
+
 }
 
-func senderMatchPGP(taskID int, taskdetails []taskDetail, taskTotal map[string]taskStat, notValidatorSender map[string]taskStat) {
-	i := 1
-	okCount := 1
-	failOunt := 1
-	for _, taskdetail := range taskdetails {
-		fmt.Printf("task%d:%d times!\n", taskID, i)
-		i++
+//将交易sender和PGP进行匹配
+//如果能匹配上，则pgp：{true,true,false,true} ,其中分别代表task1，2，3，4是否完成
+//如果匹配不上，则sender：{true,true,false,true}
+func senderMatchPGP(taskID int, taskdetails []taskDetail) {
+	repeatsender := []string{}
+	for _, taskdetail := range taskdetails {    		//遍历每次任务结果
 		ts := taskStat{}
-		if validator, ok := validatorSet[taskdetail.sender]; ok {
-			fmt.Printf("task%d:%d OKtimes!\n", taskID, okCount)
-			okCount++
-
+		if validator, ok := validatorSet[taskdetail.sender]; ok {     //能够在validator集合中找到sender
 			pgp := validator.pgp
-			if _, ok := taskTotal[pgp]; ok {
+			if _, ok := taskTotal[pgp]; ok {             //task2，3，4统计的时候需要用到task1中已经构建好的map
+				repeatsender = append(repeatsender, taskdetail.sender)
 				ts = taskTotal[pgp]
 			}
 			switch taskID {
 			case 1:
-				ts.task1 = true
+				if !ts.task1 {
+					ts.task1 = true
+				} else {
+					key := "TASK" + strconv.Itoa(taskID) + ":" + taskdetail.sender				//说明此sender已经发送过交易，本次交易重复了
+					repeatTxArr := repeatTxMap[key]
+					repeatTxArr = append(repeatTxArr, taskdetail)
+					repeatTxMap[key] = repeatTxArr
+				}
+
 			case 2:
-				ts.task2 = true
+				if !ts.task2 {
+					ts.task2 = true
+				} else {
+					key := "TASK" + strconv.Itoa(taskID) + ":" + taskdetail.sender
+					repeatTxArr := repeatTxMap[key]
+					repeatTxArr = append(repeatTxArr, taskdetail)
+					repeatTxMap[key] = repeatTxArr
+				}
 			case 3:
-				ts.task3 = true
+				if !ts.task3 {
+					ts.task3 = true
+				} else {
+					key := "TASK" + strconv.Itoa(taskID) + ":" + taskdetail.sender
+					repeatTxArr := repeatTxMap[key]
+					repeatTxArr = append(repeatTxArr, taskdetail)
+					repeatTxMap[key] = repeatTxArr
+				}
 			case 4:
-				ts.task4 = true
+				if !ts.task4 {
+					ts.task4 = true
+				} else {
+					key := "TASK" + strconv.Itoa(taskID) + ":" + taskdetail.sender
+					repeatTxArr := repeatTxMap[key]
+					repeatTxArr = append(repeatTxArr, taskdetail)
+					repeatTxMap[key] = repeatTxArr
+				}
 			}
 			taskTotal[pgp] = ts
 		} else {
-			fmt.Printf("task%d:%d FAILtimes!\n", taskID, failOunt)
-			failOunt++
 			if _, ok := notValidatorSender[taskdetail.sender]; ok {
 				ts = notValidatorSender[taskdetail.sender]
 			}
@@ -263,7 +278,24 @@ func senderMatchPGP(taskID int, taskdetails []taskDetail, taskTotal map[string]t
 			case 4:
 				ts.task4 = true
 			}
-			notValidatorSender[taskdetail.sender] = ts
+			notValidatorSender[taskdetail.sender] = ts			//不再验证人中的找不到pgp，就用sender作为key
 		}
 	}
+}
+
+
+//拉取网络资源
+func fetch(url string) ([]byte, error) {
+	client := http.Client{}
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
 }
