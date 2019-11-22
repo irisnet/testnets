9a3b36933af5b1f6c9ec7eab787e403c4597f6f7
oncloudit <47657048+oncloudit@users.noreply.github.com>
2019-11-22 11:30:27 +0800

    Update upgrade_verify.sh

--- nyancat/v0.16/upgrade_verify.sh
+++ nyancat/v0.16/upgrade_verify.sh
@@ -9,7 +9,8 @@ declare -A fva_moniker_map
 declare -A testnet_identity_map
 declare -A mainnet_identity_map
 
-### acquire validator_address of validator who signed one of blocks between 89500 and 89800 from lcd to save to hash_height_map, the height of software upgrade switched to v0.16.0-rc1 is 89500.
+### acquire validator_address of validators who has signed one of blocks between 89500 and 89800 from lcd and save it to hash_height_map, 
+### the height of software upgrade switched to version v0.16.0-rc1 is 89500.
 for i in {89501..89801}
 do
     # echo $i;
@@ -29,7 +30,7 @@ do
 done
 echo "count of hash_height_map: " ${#hash_height_map[@]}
 
-### acquire info of validators from api of explorer of nyancat and save it to fva_hash_map and testnet_identity_map.
+### acquire info of validators from api of nyancat explorer and save it to fva_hash_map and testnet_identity_map.
 curl -s 'https://nyancat.irisplorer.io/api/stake/validators?page=1&size=100&type=""&origin=browser' | jq '.Data[] | .proposer_addr+":"+.operator_address+":"+.description.identity' | sed 's/"//g' > nyancat-task-temp
 while read row; do
     # echo $row
@@ -48,7 +49,7 @@ while read row; do
     [ -n "$identity" ] && mainnet_identity_map[$identity]="ok"
 done < nyancat-task-temp
 
-### acquire voted info in proposal #1 & #3 from api of explorer of nyancat ordered by time.
+### acquire voted info in proposal #1 & #3 from api of nyancat explorer ordered by time.
 curl -s 'https://nyancat.irisplorer.io/api/gov/proposals/1/voter_txs?page=1&size=100' | jq '.items | reverse | .[] | .voter+":"+.moniker+":"+.option+"::"+.timestamp' | sed 's/"//g' > nyancat-task-temp
 curl -s 'https://nyancat.irisplorer.io/api/gov/proposals/3/voter_txs?page=1&size=100' | jq '.items | reverse | .[] | .voter+":"+.moniker+":"+.option+"::"+.timestamp' | sed 's/"//g' >> nyancat-task-temp
 index=0
@@ -58,13 +59,13 @@ while read row; do
     moniker=$(echo $row | cut -d':' -f2)
     vote=$(echo $row | cut -d':' -f3)
     timestamp=$(echo $row | awk -F:: '{print $2}')
-    ### filter validator of duplicate vote and unsigned one of 300 blocks after software upgrade.
+    ### filter validators of duplicate vote and unsigned one of 300 blocks after software upgrade.
     if [ -z "${fva_vote_map[$fva]}" -a -n "${fva_hash_map[$fva]}" ] && [ -n "${hash_height_map[${fva_hash_map[$fva]}]}" ]; then
         if [ $index -lt 40 ]; then
             index=$[$index + 1]
             fva_vote_map[$fva]=$vote
             echo -e $index"\t"$fva"\t"$timestamp"\t"${testnet_identity_map[$fva]}"\t"$moniker
-        ### validator of mainnet beyond top 40 is passed.
+        ### validators of mainnet beyond top 40 is passed by verifying their identity.
         elif [ -n "${testnet_identity_map[$fva]}" ] && [ "${mainnet_identity_map[${testnet_identity_map[$fva]}]}" == "ok" ]; then
             index=$[$index + 1]
             fva_vote_map[$fva]=$vote
